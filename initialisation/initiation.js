da092eb1d4fbe29b5bcedd26225082d3fd98daa7
Denis Boogaerts Afelio <d.boogaerts@afelio.be>
2019-06-11 13:06:15 +0200

    first commit

---
+++ initialisation/initiation.js
@@ -0,0 +1,8 @@
+
+document.forms[0].ch1.addEventListener('focus', () => {        
+    document.forms[0].ch1.value="";
+})
+
+document.forms[1].ch1.addEventListener('focus', () => {
+    document.forms[1].ch1.value="";
+})
